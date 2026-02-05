6aaff648be3633a84103c922b3e44e30e1ebc1a3
Donald Filimon <cbkshadow@icloud.com>
2026-02-05 13:33:21 -0500

    chore: use similarity-search-kit fork with resources

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .package(url: "https://github.com/colinc86/LaTeXSwiftUI", branch: "main"),
         .package(url: "https://github.com/sindresorhus/LaunchAtLogin-Modern", branch: "main"),
         .package(url: "https://github.com/vpeschenkov/SecureDefaults", from: "1.2.2"),
-        .package(url: "https://github.com/johnbean393/similarity-search-kit", branch: "main"),
+        .package(url: "https://github.com/donaldfilimon/similarity-search-kit", branch: "mlai-resources"),
         .package(url: "https://github.com/JohnSundell/Splash", from: "0.16.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift", from: "0.15.4"),
         .package(url: "https://github.com/SwiftfulThinking/SwiftfulLoadingIndicators", from: "0.0.4"),
